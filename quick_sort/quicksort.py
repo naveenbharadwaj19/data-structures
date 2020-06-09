7cdb361e020d1781dcbfa16e35ed4554bb6f46bd
naveen bharadwaj <bharadwajpal22@gmail.com>
2020-06-09 18:45:31 +0530

    added quick sort

---
+++ quick_sort/quicksort.py
@@ -0,0 +1,26 @@
+from random import randrange, shuffle
+def quicksort(list, start, end):
+    if start >= end:
+        return
+
+    pivot_idx = randrange(start, end + 1)
+    pivot_element = list[pivot_idx]
+    list[end], list[pivot_idx] = list[pivot_idx], list[end]
+    less_than_pointer = start
+
+    for i in range(start, end):
+        if list[i] < pivot_element:
+            list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+            less_than_pointer += 1
+
+    list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
+    quicksort(list, start, less_than_pointer - 1)
+    quicksort(list, less_than_pointer + 1, end)
+
+
+
+list_ = [14,33,27,35,10]
+shuffle(list_)
+quicksort(list_, 0, len(list_) - 1)
+print(list_)
+
